9c59b88b75dce6e86eaef9a921325565c412710c
Amal nath <okteto66@gmail.com>
2022-12-03 16:37:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,7 +25,6 @@ BOT_TOKEN = environ['BOT_TOKEN']
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
-PICS = (environ.get('PICS' ,'https://telegra.ph/file/2e2a07e86066538ed7406.jpg')).split()
 BOT_START_TIME = time()
 
 # Admins, Channels & Users
